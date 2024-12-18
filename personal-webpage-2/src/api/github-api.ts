bdbce95b55adbdbe712277f72a27f161b579ed09
William <williamnorland@gmail.com>
2024-12-18 11:27:13 +0100

    api script

---
+++ personal-webpage-2/src/api/github-api.ts
@@ -0,0 +1,35 @@
+import { Octokit } from "octokit"
+
+// Fetches github repos using access token from .env.local, this is only for a single user and not for a general search
+export async function fetchMyRepos(): Promise<[Repo]> {
+
+  const octokit = new Octokit({
+
+    auth: process.env.GITHUB_ACCESS_TOKEN
+
+  })
+
+  const endpoint: string = 'GET /users/{username}/repos'
+
+  const response = await octokit.request(endpoint, {
+
+    username: 'willayy',
+
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+
+  })
+
+  return JSON.parse(JSON.stringify(response.data))
+
+}
+
+export type Repo = {
+  id : number,
+  name: string
+  description: string,
+  topics: string[],
+  created_at: string,
+  updated_at: string,
+}
