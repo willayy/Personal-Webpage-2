94a6fba15009a3d3ecf0016193fcaac66ecb7246
William Norland <william@luwisoft.com>
2025-01-04 19:21:02 -0500

    Career page

--- personal-webpage-2/src/app/mycareer/page.tsx
+++ personal-webpage-2/src/app/mycareer/page.tsx
@@ -1,45 +1,74 @@
 import EmploymentListItem from '@/components/EmploymentListItem/EmploymentListItem';
-import { Employment } from '@/utils/employmentType';
+import { Employment, Education } from '@/utils/careerTypes';
 import List from '@/components/List/List';
+import EducationListItem from '@/components/EducationListItem/EducationListItem';
 
 const listBox = 'listBox'
 
 export default async function Projects() {
 
-    const data: [Employment] = await fetch('/resources/employments.json')
-    .then((res) => res.json())
-    .then((json) => JSON.parse(JSON.stringify(json)))
+    const baseUrl = process.env.PUBLIC_SITE_URL
+
+    const fetchAndParse = async (url: string) => {
+        return await fetch(url).then(res => {
+                if (res.ok) {
+                    return res.json()
+                }
+            }
+        )
+    }
+
+    const employmentData: [Employment] = await fetchAndParse(baseUrl + '/data/employments.json')
+
+    const educationData: [Education] = await fetchAndParse(baseUrl + '/data/education.json')
 
     return (
 
         <main>
+            
+            <div className={ listBox + " roboto-regular"}>
 
-        <div className={ listBox + " roboto-regular"}>
+                <h1> Education </h1>
 
-            <h1> Employments </h1>
+                <List> {
 
-            <List>
+                    educationData.map((education) => (
 
-            {
+                        <EducationListItem
+                            school={education.school}
+                            degree={education.degree}
+                            graduation={education.graduation}
+                            key={education.id}
+                        />
 
-                data.map((employment) => (
+                    ))
 
-                    <EmploymentListItem
-                        position={employment.position}
-                        organization={employment.organization}
-                        startDate={employment.startDate}
-                        endDate={employment.endDate}
-                        description={employment.description}
-                        key={employment.id}
-                    />
+                } </List>
 
-                ))
+            </div>
 
-            }
+            <div className={ listBox + " roboto-regular"}>
 
-            </List>
+                <h1> Employments </h1>
 
-        </div>
+                <List> {
+
+                    employmentData.map((employment) => (
+
+                        <EmploymentListItem
+                            position={employment.position}
+                            organization={employment.organization}
+                            startDate={employment.startDate}
+                            endDate={employment.endDate}
+                            description={employment.description}
+                            key={employment.id}
+                        />
+
+                    ))
+
+                } </List>
+
+            </div>
 
         </main>
 
