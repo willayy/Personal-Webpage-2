67ef5d539c2bfafe7efa7314811e28c0baf10a00
William Norland <william@luwisoft.com>
2025-01-04 23:14:18 -0500

    add contact page

---
+++ personal-webpage-2/src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+export default async function Contact() {
+    return (
+        <main>
+            <div className = { "listBox " + "roboto-regular"}>
+                <h3 > Mail </h3>
+                <a href="mailto:williamnorland@gmail.com"> williamnorland@gmail.com </a>
+            </div>
+        </main>
+    );
+}
