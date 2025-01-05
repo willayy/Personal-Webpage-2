6de4e3eae612744483907564804ac5eb672a053d
William Norland <william@luwisoft.com>
2025-01-05 13:59:54 -0500

    fix bugs

--- personal-webpage-2/next.config.mjs
+++ personal-webpage-2/next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
     unoptimized: true, // Disable default image optimization
   },
   assetPrefix: isProd ? 'Personal-Webpage-2/' : '',
-  basePath: isProd ? '/personal-webpage-2' : '',
+  basePath: isProd ? '/Personal-Webpage-2' : '',
   output: 'export'
 };
 
