5e0df90ff946fd2de7d2baa5531789ecb716f75f
William Norland <william@luwisoft.com>
2025-01-06 21:28:41 +0100

    fix conf assetPrefix

--- personal-webpage-2/next.config.mjs
+++ personal-webpage-2/next.config.mjs
@@ -6,8 +6,8 @@ const nextConfig = {
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? 'Personal-Webpage-2/' : '',
-  basePath: isProd ? '/Personal-Webpage-2/personal-webpage-2' : '',
+  assetPrefix: isProd ? './' : '',
+  basePath: isProd ? '/Personal-Webpage-2' : '',
   output: 'export'
 };
 
