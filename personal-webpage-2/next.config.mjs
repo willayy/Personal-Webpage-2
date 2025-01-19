00b12e78eb62c3f49537e74e6a94bba68a960b45
William Norland <williamnorland@gmail.com>
2025-01-19 23:57:55 +0100

    fix

--- personal-webpage-2/next.config.mjs
+++ personal-webpage-2/next.config.mjs
@@ -1,8 +1,12 @@
-// @ts-check
- 
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  /* config options here */
-}
- 
-module.exports = nextConfig
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? './' : '',
+  basePath: isProd ? '/Personal-Webpage-2' : ''
+};
+export default nextConfig;
